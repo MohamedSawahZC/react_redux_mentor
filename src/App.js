1261b8cd0d735c92bb2b8b74326b4277f527a339
Mohamed Sawah <sawah810@gmail.com>
2022-10-09 12:15:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <h3>Counter : {data}</h3>
       <h3>Log Status : {`${isLog}`}</h3>
-      <button onClick={() => dispathCounter({ type: "INCREMENT" })}>
+      <button onClick={() => dispathCounter({ type: "I NCREMENT" })}>
         Increase
       </button>
       <button onClick={() => dispathCounter({ type: "DECREMENT" })}>
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { legacy_createStore as createStore } from 'redux';
 import { rootReducer } from '../reducer/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
 //1-Create Store
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer,composeWithDevTools());
